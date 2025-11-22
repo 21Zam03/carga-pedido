055113a53303fd8ae8ae8c96ad66fef162f5ff3d
Jose Zambrano <21zam03.free@gmail.com>
2025-11-21 20:59:31 -0500

    initial commit

---
+++ src/main/java/com/dinet/carga_pedido/CargaPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.dinet.carga_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinet/carga_pedido/CargaPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinet.carga_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
